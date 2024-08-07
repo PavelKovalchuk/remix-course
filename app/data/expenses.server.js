1a7d976b92be9cd94ade19bb17c05956f281ca80
pkovalchuk <pavel.kovalchuk@dataart.com>
2024-08-07 15:28:00 +0200

    feat: update and delete

--- app/data/expenses.server.js
+++ app/data/expenses.server.js
@@ -36,3 +36,30 @@ export async function getExpense(id) {
     throw error;
   }
 }
+
+export async function updateExpense(id, expenseData) {
+  try {
+    await prisma.expense.update({
+      where: { id },
+      data: {
+        title: expenseData.title,
+        amount: +expenseData.amount,
+        date: new Date(expenseData.date),
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function deleteExpense(id) {
+  try {
+    await prisma.expense.delete({
+      where: { id },
+    });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
