2103c11ae19c9d2556e18cc338bc9fbd07ee7d0f
pkovalchuk <pavel.kovalchuk@dataart.com>
2024-08-08 11:00:05 +0200

    feat: error handling in the route

--- app/components/expenses/ExpenseForm.jsx
+++ app/components/expenses/ExpenseForm.jsx
@@ -2,7 +2,7 @@ import {
   Form,
   Link,
   useActionData,
-  // useLoaderData,
+  useLoaderData,
   useMatches,
   useParams,
   useNavigation,
@@ -16,6 +16,12 @@ function ExpenseForm() {
   const matches = useMatches();
   const expenses = matches.find((match) => match.id === 'routes/__app/expenses').data;
   const expenseData = expenses.find((expense) => expense.id === params.id);
+
+  if (params.id && !expenseData) {
+    // throw new Response();
+    return <p>Invalid expense id.</p>;
+  }
+
   const navigation = useNavigation();
 
   const defaultValues = expenseData
--- app/routes/__app/expenses.jsx
+++ app/routes/__app/expenses.jsx
@@ -1,5 +1,4 @@
 // /expenses => shared layout
-import { json } from '@remix-run/node';
 import { Link, Outlet, useLoaderData } from '@remix-run/react';
 import { FaPlus, FaDownload } from 'react-icons/fa';
 
@@ -8,6 +7,8 @@ import { getExpenses } from '~/data/expenses.server';
 
 export default function ExpensesLayout() {
   const expenses = useLoaderData();
+
+  const hasExpenses = expenses && expenses.length > 0;
 
   return (
     <>
@@ -23,17 +24,32 @@ export default function ExpensesLayout() {
             <span>Load Raw Data</span>
           </a>
         </section>
-        <ExpensesList expenses={expenses} />
+        {hasExpenses && <ExpensesList expenses={expenses} />}
+        {!hasExpenses && (
+          <section id="no-expenses">
+            <h1>No expenses found</h1>
+            <p>
+              Start <Link to="add">adding some</Link> today.
+            </p>
+          </section>
+        )}
       </main>
     </>
   );
 }
 
-export function loader() {
-  return getExpenses();
+export async function loader() {
+  const expenses = await getExpenses();
+  return expenses;
+
+  // if (!expenses || expenses.length === 0) {
+  //   throw json(
+  //     { message: 'Could not find any expenses.' },
+  //     { status: 404, statusText: 'No expenses found' }
+  //   );
+  // }
 }
 
-export async function loaderWithJson() {
-  const expenses = await getExpenses();
-  return json(expenses);
-}
+// export function CatchBoundary() {
+//   return <p>Error</p>
+// }
